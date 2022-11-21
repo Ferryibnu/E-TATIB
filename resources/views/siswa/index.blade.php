8e652318e4ebffb837846aff2f2d7fbe43d02cb2
Ferry Ibnu <ferryibnu86@gmail.com>
2022-11-21 12:18:41 +0700

    Update Profile

--- resources/views/siswa/index.blade.php
+++ resources/views/siswa/index.blade.php
@@ -215,7 +215,7 @@
               {{-- End Modal Delete --}}
 
               {{-- Modal Edit --}}
-              <div class="modal fade" id="ModalEdit{{$s->id}}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+              <div class="modal fade" id="ModalEdit{{$s->id}}" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
                 <div class="modal-dialog" role="document">
                   <div class="modal-content">
                     <div class="modal-header">
--- resources/views/siswa/profile.blade.php
+++ resources/views/siswa/profile.blade.php
@@ -4,57 +4,57 @@
 <section class="content">
   <div class="container-fluid">
     <div class="row justify-content-center">
-        <div class="col-9 col-md-10">
+        <div class="col-12 col-md-10">
           <div class="card shadow mb-4 mt-4">
             <div class="card-header">
               <h5 class="m-0 font-weight-bold text-dark text-center">{{$siswa->nama}}</h5>
             </div>
             <div class="card-body">
-              <div class="row ml-5">
-                  <div class="col-md-4"> NISN </div>
-                  <div class="col-md-8"> :&emsp;{{$siswa->nisn}}</div>
+              <div class="row">
+                  <div class="col-4"> NISN </div>
+                  <div class="col-8"> :&emsp;{{$siswa->nisn}}</div>
               </div>
               <hr>
 
-              <div class="row ml-5">
-                <div class="col-md-4"> Nama </div>
-                <div class="col-md-8"> :&emsp;{{$siswa->nama}}</div>
+              <div class="row">
+                <div class="col-4"> Nama </div>
+                <div class="col-8"> :&emsp;{{$siswa->nama}}</div>
               </div>
               <hr>
 
-              <div class="row ml-5">
-                <div class="col-md-4"> Tempat, Tanggal Lahir</div>
-                <div class="col-md-8"> :&emsp;{{$siswa->tempat_lahir}} {{isset($siswa->tgl_lahir) ? ', '.date('d F Y', strtotime($siswa->tgl_lahir)) : ''}}</div>
+              <div class="row">
+                <div class="col-4"> Tempat, Tgl Lahir</div>
+                <div class="col-8"> :&emsp;{{$siswa->tempat_lahir}} {{isset($siswa->tgl_lahir) ? ', '.date('d F Y', strtotime($siswa->tgl_lahir)) : ''}}</div>
               </div>
               <hr>
 
-              <div class="row ml-5">
-                <div class="col-md-4"> Kelas</div>
-                <div class="col-md-8"> :&emsp;{{ $siswa->kelas->kelas }}</div>
+              <div class="row">
+                <div class="col-4"> Kelas</div>
+                <div class="col-8"> :&emsp;{{ $siswa->kelas->kelas }}</div>
               </div>
               <hr>
 
-              <div class="row ml-5">
-                <div class="col-md-4"> Jenis Kelamin</div>
-                <div class="col-md-8"> :&emsp;@if($siswa->jns_kelamin == 'L') Laki-laki @else Perempuan @endif</div>
+              <div class="row">
+                <div class="col-4"> Jenis Kelamin</div>
+                <div class="col-8"> :&emsp;@if($siswa->jns_kelamin == 'L') Laki-laki @else Perempuan @endif</div>
               </div>
               <hr>
 
-              <div class="row ml-5">
-                <div class="col-md-4"> Agama</div>
-                <div class="col-md-8"> :&emsp;{{ $siswa->agama }}</div>
+              <div class="row">
+                <div class="col-4"> Agama</div>
+                <div class="col-8"> :&emsp;{{ $siswa->agama }}</div>
               </div>
               <hr>
 
-              <div class="row ml-5">
-                <div class="col-md-4"> No HP</div>
-                <div class="col-md-8"> :&emsp;{{ $siswa->no_telp }}</div>
+              <div class="row">
+                <div class="col-4"> No HP</div>
+                <div class="col-8"> :&emsp;{{ $siswa->no_telp }}</div>
               </div>
               <hr>
 
-              <div class="row ml-5">
-                <div class="col-md-4"> Penghargaan</div>
-                <div class="col-md-8"> :&emsp;{{isset($siswa->penghargaan->kriteria) ? $siswa->penghargaan->kriteria. ' ('.$siswa->penghargaan->poin. ' poin)' : ''}}</div>
+              <div class="row">
+                <div class="col-4"> Penghargaan</div>
+                <div class="col-8"> :&emsp;{{isset($siswa->penghargaan->kriteria) ? $siswa->penghargaan->kriteria. ' ('.$siswa->penghargaan->poin. ' poin)' : ''}}</div>
               </div>
               <hr>
         
@@ -68,7 +68,7 @@
     </div> 
 
     <div class="row justify-content-center tabel-pel">
-      <div class="col-9 col-md-10">
+      <div class="col-12 col-md-10">
         <div class="card shadow mb-4 mt-4">
           <div class="card-header">
             <h5 class="m-0 font-weight-bold text-dark text-center">Data Pelanggaran</h5>
